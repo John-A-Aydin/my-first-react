b6bf482d65bcbc946186c13271b18bda0daab7ef
John-A-Aydin <johnaaydin@gmail.com>
2023-05-18 10:51:36 -0400

    Added functionality to Navbar

--- comps/Navbar.tsx
+++ comps/Navbar.tsx
@@ -1,14 +1,18 @@
+import Link from "next/link";
+
+
 function Destination(name:string, link:string) {
     return (
-        <h1 className="flex justify-end p-2 text-xl font-mono hover:text-gray-950">{name}</h1>
+        <Link href={link}><h1 className="flex justify-end p-2 text-xl font-mono hover:text-gray-950">{name}</h1></Link>
     );
 }
 export default function Navbar() {
     return (
         <div className="w-screen h-12 fixed top-0 flex justify-end bg-gray-800 border-gray-950">
-           {Destination("Projects", "TODO")}
-           {Destination("Resume", "TODO")}
-           {Destination("About", "TODO")}
+            {Destination("Home", "/")}
+            {Destination("Projects", "TODO")}
+            {Destination("Resume", "TODO")}
+            {Destination("About", "/about")}
         </div>
     );
 }
---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import Navbar from "@/comps/Navbar";
+export default function About() {
+    return (
+        <div>
+            <Navbar/>
+            <h1>
+                Hello
+            </h1>
+        </div>
+    );
+}
